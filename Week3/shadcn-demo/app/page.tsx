dc320777a81c0b18007207db60bc64fb1accfd4d
Joshua Emery <102991550+EmeryAtRTC@users.noreply.github.com>
2025-04-22 12:59:23 -0700

    Added shadcn demo

---
+++ Week3/shadcn-demo/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import MyCard from "./components/MyCard";
+export default function Page() {
+  return (
+    <div className="min-h-screen border-2 border-amber-950 rounded-lg">
+      <MyCard></MyCard>
+    </div>
+  );
+}
